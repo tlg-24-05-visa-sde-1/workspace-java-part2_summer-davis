cbe40ffbba85fecfbc9819a02111e2c4b96b4071
Summer Davis <summerd@tlgcohort.com>
2024-06-07 13:33:51 -0500

    findByBrand() / findbyBrands() methods and unit tests

--- Lab04.1/src/com/entertainment/catalog/Catalog.java
+++ Lab04.1/src/com/entertainment/catalog/Catalog.java
@@ -28,16 +28,29 @@ public class Catalog {
      * A no-matches result should be an empty collection (not null).
      */
     public static Collection<Television> findByBrand(String brand) {
-        return null;
+        Collection<Television> result = new ArrayList<>();
+
+        for (Television tv : catalog) {
+            if (tv.getBrand().equals(brand)) {
+                result.add(tv);
+            }
+        }
+        return result;
     }
 
     /**
-     * Searches catalog by one or more brands, and returns a map with an entry for each brand supplied,
+     * Searches catalog by one or more brands, and returns a map with an entry (row) for each brand supplied,
      * with a corresponding collection of matching Televisions for that brand.
      * A no-brands-passed result should be an empty map (not null).
      */
     public static Map<String,Collection<Television>> findByBrands(String... brands) {
-        return null;
+        Map<String,Collection<Television>> result = new HashMap<>();
+
+        for (String brand : brands) {
+            Collection<Television> entry = findByBrand(brand);
+            result.put(brand, entry);
+        }
+        return result;
     }
 
     /**
