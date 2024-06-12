8370a38233e596571002a0290065cef395be99e5
Summer Davis <summerd@tlgcohort.com>
2024-06-12 13:56:57 -0500

    sorts via anonymous Comparator classes

--- Scratch/src/com/crisp/RadishSortTest.java
+++ Scratch/src/com/crisp/RadishSortTest.java
@@ -1,6 +1,7 @@
 package com.crisp;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 class RadishSortTest {
@@ -23,11 +24,32 @@ class RadishSortTest {
     dump(radishes);
 
     System.out.println("sort by color via RadishColorComparator");
-    radishes.sort(new RadishColorComparator());
+    //radishes.sort(new RadishColorComparator());
+    radishes.sort(new Comparator<Radish>() {
+      @Override
+      public int compare(Radish radish1, Radish radish2) {
+        return radish1.getColor().compareTo(radish2.getColor());
+      }
+    });
     dump(radishes);
 
     System.out.println("sort by guysOnTop via RadishGuysOnTopComparator");
-    radishes.sort(new RadishGuysOnTopComparator());
+    //radishes.sort(new RadishGuysOnTopComparator());
+    radishes.sort(new Comparator<Radish>() {
+      @Override
+      public int compare(Radish radish1, Radish radish2) {
+        return Integer.compare(radish1.getGuysOnTop(), radish2.getGuysOnTop());
+      }
+    });
+    dump(radishes);
+
+    System.out.println("sort by tailLength via anonymous class");
+    radishes.sort(new Comparator<Radish>() {
+      @Override
+      public int compare(Radish radish1, Radish radish2) {
+        return Double.compare(radish1.getTailLength(), radish2.getTailLength());
+      }
+    });
     dump(radishes);
   }
 
