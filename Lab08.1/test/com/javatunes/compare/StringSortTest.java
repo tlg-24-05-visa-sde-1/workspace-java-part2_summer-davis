1ef79991e53d301de58b46ba49a29b90029a3d96
Summer Davis <summerd@tlgcohort.com>
2024-06-13 13:35:41 -0500

    Lab 8.1 - sorts via lambda as Comparator

--- Lab08.1/test/com/javatunes/compare/StringSortTest.java
+++ Lab08.1/test/com/javatunes/compare/StringSortTest.java
@@ -14,24 +14,27 @@ import org.junit.Test;
 
 public class StringSortTest {
 
-    private List<String> names = Arrays.asList("Jason", "Martin", "Jack", "Mary", "Jill", "Frederick", "Laurie", "Ed");
+  private final List<String> names =
+      Arrays.asList("Jason", "Martin", "Jack", "Mary", "Jill", "Frederick", "Laurie", "Ed");
 
-    @Test
-    public void testStringListSort() {
-        System.out.println("Order will be added to following list:");
-        System.out.println(names + "\n");
+  @Test
+  public void testStringListSort() {
+    System.out.println("Order will be added to following list:");
+    System.out.println(names + "\n");
 
-        System.out.println("Natural order:");
-        names.sort(null);
-        System.out.println(names + "\n");
+    System.out.println("Natural order:");
+    names.sort(null);
+    System.out.println(names + "\n");
 
-        System.out.println("Increasing length - Comparator class:");
-        names.sort(new StringLengthComparator());
-        System.out.println(names + "\n");
+    System.out.println("Increasing length - Comparator class:");
+    names.sort(new StringLengthComparator());
+    System.out.println(names + "\n");
 
-        System.out.println("Increasing length - lambda:");
-        // TODO: call names.sort() and pass in an "expression" lambda
-        // hint: a compatible lambda will have this form:
-        //       (obj1, obj2) -> expression-that-evaluates-to-int
-    }
+    System.out.println("Increasing length - lambda:");
+    // call names.sort() and pass in an "expression" lambda
+    // hint: a compatible lambda will have this form:
+    //       (obj1, obj2) -> expression-that-evaluates-to-int
+    names.sort((name1, name2) -> Integer.compare(name2.length(), name1.length()));
+    System.out.println(names);
+  }
 }
